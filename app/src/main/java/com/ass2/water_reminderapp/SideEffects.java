f5aa7215fe91955ea441ff2030c8fb97a81a101b
usman182 <i190455@nu.edu.pk>
2022-12-07 20:34:28 +0500

    My Commit

--- app/src/main/java/com/ass2/water_reminderapp/SideEffects.java
+++ app/src/main/java/com/ass2/water_reminderapp/SideEffects.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SideEffects extends AppCompatActivity {
 
     Button back;
+    TextView txtview1, txtview2, txtview3, txtview4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +24,15 @@ public class SideEffects extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), Activity3.class));
             }
         });
+
+        txtview1 = findViewById(R.id.txtview1);
+        txtview2 = findViewById(R.id.txtview2);
+        txtview3 = findViewById(R.id.txtview3);
+        txtview4 = findViewById(R.id.txtview4);
+
+        txtview1.setText("May Cause Diarrhea");
+        txtview2.setText("Overburdens The Heart");
+        txtview3.setText("May Cause Liver Problems");
+        txtview3.setText("Causes The Cells To Swell Up");
     }
 }
